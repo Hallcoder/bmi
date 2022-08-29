02c9c291e93bd725a5f772330a52bc624f5b4cbf
Hallcoder <hallcoder25@gmail.com>
2022-08-29 20:55:11 +0200

    Customize the look of your app at app level and granular level using theme property and themeData widget or theme widget with the data property

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,17 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        //you can use ThemeData.dark() and customize some small different part you want using copyWith()
+        textTheme: const TextTheme(
+          bodyText2:TextStyle(
+            color: Colors.white,
+          ),
+          ),
+        scaffoldBackgroundColor: Color(0xff0a0e21),
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(primary: Color(0xff0a0e21), secondary: Colors.purple),
+      ),
       home: InputPage(),
     );
   }
@@ -21,13 +32,14 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: const Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Body Text'),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
